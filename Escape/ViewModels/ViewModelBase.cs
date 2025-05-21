1059ecd780730202de0b0276e0cab229f1f7955a
Yerko Andrei <yerkoandrei@gmail.com>
2025-05-21 14:53:16 -0400

    primer commit

---
+++ Escape/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Escape.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Escape/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Escape.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
