aefbe6c9f514854ac7245f7c285276f72b090484
Yerko Andrei <yerkoandrei@gmail.com>
2025-06-07 18:28:28 -0400

    preparando archivos

---
+++ Escape/Constantes.cs
@@ -0,0 +1,26 @@
+public class Constantes
+{
+    private const string ISO8006 = "yyyy-MM-ddThh:mm:ss";
+
+    public enum Personajes
+    {
+        pensamiento,
+        jugador,
+        personajeA,
+        personajeB,
+        personajeC,
+    }
+
+    public enum Idiomas
+    {
+        sistema,
+        español,
+        inglés
+    }
+
+    public enum Configuraciones
+    {
+        idioma,
+        sonido
+    }
+}
---
+++ Escape/Guión.cs
@@ -0,0 +1,26 @@
+public class Guión
+{
+    // Introducción
+    public void ObtenerIntro()
+    {
+        
+    }
+
+    // Ruta A
+    public void ObtenerRutaA()
+    {
+
+    }
+
+    // Ruta B
+    public void ObtenerRutaB()
+    {
+
+    }
+
+    // Ruta C
+    public void ObtenerRutaC()
+    {
+
+    }
+}
